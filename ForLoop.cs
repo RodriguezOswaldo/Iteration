80ae4df123a6c4deda36df59ac84b3ef76767649
RodriguezOswaldo <owner2303@gmail.com>
2019-10-15 15:34:43 -0400

    comments

--- ForLoop.cs
+++ ForLoop.cs
@@ -9,21 +9,35 @@ namespace Iteration
         public static void moreLoops()
         {
             Console.WriteLine("\n");
+            Console.WriteLine("Using For Loops");
+
             var name = "John Smith";
             for (var i = 0; i < name.Length; i++)
             { 
                 Console.WriteLine(name[i]);
             }
+            Console.WriteLine("Using ForEach Loops");
+
             //this one those the exact same thing
             foreach (var character in name)
             {
                 Console.WriteLine(character);
             }
             //Iterate over every number
+            Console.WriteLine("Using ForEach Loops");
+
             var numbers = new int[] {1, 2, 3, 4, 5, 6};
             foreach (var number in numbers)
             {
                 Console.WriteLine(number + 9);
+            }
+            Console.WriteLine("Using While Loops");
+            var ii = 0;
+            while (ii < 10)
+            {
+                if(ii% 2 ==0)
+                    Console.WriteLine(ii);
+                ii++;
             }
         }
     }
