10bb8513c7e871784109160124ad0f6d0ea6f314
RodriguezOswaldo <owner2303@gmail.com>
2019-10-15 15:51:47 -0400

    Random Number / moving iteration to a new class and calling it in the main method

---
+++ RandomNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iteration
+{
+    class RandomNumbers
+    {
+        public static void newRandom()
+        {
+            var random = new Random();
+            int number = random.Next(1, 20);
+            for (var i = 0; i != number; i++)
+                Console.WriteLine(i);
+                Console.WriteLine("no the number");
+                
+        }
+    }
+}
---
+++ SimpleLoops.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iteration
+{
+    class SimpleLoops
+    {
+        public static void Loops()
+        {
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 != 0)
+                    Console.WriteLine(i);
+            }
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 != 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
